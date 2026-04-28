b9d540d600b9668b2ece036d09d538d17431daa9
ravish-zenteiq-ai <ravish@zenteiq.com>
2026-04-28 17:08:29 +0530

    text

--- app/main.py
+++ app/main.py
@@ -6,7 +6,10 @@ from fastapi import FastAPI, status, HTTPException
 from sqlalchemy.orm import Session
 import app.models.model as model
 from app.db.base import Base, engine, SessionLocal
-from app.schemas.schemas import createPost
+from app.schemas.schemas import (
+    Post,
+    createPost
+)
 
 
 model.Base.metadata.create_all(bind=engine)
@@ -36,22 +39,20 @@ async def posts(db: Session = Depends(get_db)):
     return{"message": db.query(model.Post).all()}
 
 
-
-
-
-@app.post("/post")
+@app.post("/post", response_model=Post)
 async def create_post(post: createPost,db: Session =  Depends(get_db)):
     newPost = model.Post(title = post.title, description= post.description)
     db.add(newPost)
     db.commit()
     db.refresh(newPost)
-    return{"message": newPost}
-
+    # return{"message": newPost}
+    return newPost
 @app.get("/post/{id}")
 async def get_post(id: int, db: Session = Depends(get_db)):
     one_post = db.query(model.Post).filter(model.Post.id == id).first()   #.all() is also there but the thing is only one post will be there of 1 id it will onl;y take tyime in db to search another post of that id i.e time wate onl;y
     if not one_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This post is not available")
+    # return{"message": one_post}
     return{"message": one_post}
 
 @app.put("/post/{id}")
@@ -68,4 +69,5 @@ async def dlt_post(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="POst not found")
     delete_post.delete(synchronize_session=False)
     db.commit()
-    return{"message": f"Post {delete_post} is deleted"}
+    # return{"message": f"Post {delete_post} is deleted"}
+    return  delete_post
--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,16 +1,25 @@
+
+from __future__ import annotations #store type hint as strings instead of acutual objects ( we can use title: name => even if name is not defined yet)
+from datetime import datetime
 from pydantic import BaseModel, Field
 
 
 
 
 class createPost(BaseModel):
-    title: str
-    description: str
+    title: str = Field(min_length=3)
+    description: str = Field(max_length=30)      #for number we can use gt and lt liek Field(gt=0, lt=2650)
     is_published: bool = True
-
-class updatePost(createPost):
-    pass
-
-class newPost(createPost):
-    is_new: bool = True
     
+
+# class updatePost(createPost):
+#     pass
+
+# class newPost(createPost):
+#     is_new: bool = True
+
+class Post(createPost):
+    # title: str
+    # description: str
+    created_at: datetime
+    # is_published: bool
